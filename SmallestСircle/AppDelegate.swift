96908e67fe01f021f5b6b4fc29ca996f09f83639
Vladyslav Yakovlev <Vlad@macbook-air.local>
2018-04-25 02:17:27 +0300

    Initial Commit

---
+++ SmallestСircle/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SmallestСircle
+//
+//  Created by Vladyslav Yakovlev on 25.04.2018.
+//  Copyright © 2018 Vladyslav Yakovlev. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
